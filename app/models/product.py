35b172df91ccd40e5fe69521a853eabf0175c3ba
Lucas Alcoléa Oue <lucasoue11@gmail.com>
2024-08-24 17:51:30 +0900

    English update

--- app/models/product.py
+++ app/models/product.py
@@ -1,54 +1,55 @@
 from .. import mongo
 
-class ProdutoAgricola:
-    def __init__(self, nome, descricao, preco, estoque, image_url=None):
-        self.nome = nome
-        self.descricao = descricao
-        self.preco = preco
-        self.estoque = estoque
+
+class AgriculturalProduct:
+    def __init__(self, name, description, price, stock, image_url=None):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.stock = stock
         self.image_url = image_url
 
-    # Função para criar uma instância da classe ProdutoAgricola a partir de um dicionário
+    # Função para criar uma instância da classe AgriculturalProduct a partir de um dicionário.
     @staticmethod
     def from_dict(data):
-        return ProdutoAgricola(
-            nome=data.get("nome"),
-            descricao=data.get("descricao"),
-            preco=data.get("preco"),
-            estoque=data.get("estoque"),
+        return AgriculturalProduct(
+            name=data.get("name"),
+            description=data.get("description"),
+            price=data.get("price"),
+            stock=data.get("stock"),
             image_url=data.get("image_url")
         )
 
-    # Função para transformar o objeto ProdutoAgricola em um dicionário
+    # Function to convert the AgriculturalProduct object into a dictionary
     def to_dict(self):
         return {
-            "nome": self.nome,
-            "descricao": self.descricao,
-            "preco": self.preco,
-            "estoque": self.estoque,
+            "name": self.name,
+            "description": self.description,
+            "price": self.price,
+            "stock": self.stock,
             "image_url": self.image_url
         }
 
     @staticmethod
-    def obter_todos_produtos():
-        return mongo.db.produtos.find()
+    def get_all_products():
+        return mongo.db.products.find()
 
     @staticmethod
-    def obter_produto_por_id(produto_id):
-        return mongo.db.produtos.find_one({"_id": produto_id})
+    def get_product_by_id(product_id):
+        return mongo.db.products.find_one({"_id": product_id})
 
     @staticmethod
-    def criar_produto(dados_produto):
-        return mongo.db.produtos.insert_one(dados_produto)
+    def create_product(product_data):
+        return mongo.db.products.insert_one(product_data)
 
     @staticmethod
-    def atualizar_produto(produto_id, dados_produto):
-        return mongo.db.produtos.update_one({"_id": produto_id}, {"$set": dados_produto})
+    def update_product(product_id, product_data):
+        return mongo.db.products.update_one({"_id": product_id}, {"$set": product_data})
 
     @staticmethod
-    def deletar_produto(produto_id):
-        return mongo.db.produtos.delete_one({"_id": produto_id})
+    def delete_product(product_id):
+        return mongo.db.products.delete_one({"_id": product_id})
     
     @staticmethod
-    def obter_produtos_em_estoque():
-        return mongo.db.produtos.find({"estoque": {"$gt": 0}})
+    def get_products_in_stock():
+        return mongo.db.products.find({"stock": {"$gt": 0}})
